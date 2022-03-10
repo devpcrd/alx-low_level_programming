43fec0b12da65bbf7b97d024cda3a94afb790b37
root <PCRepairDoctor>
2022-03-10 09:13:55 -0800

    fysyj

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
-int main() {
+int main(void)
+	{
 
-	puts("Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
-}
+		return (0);
+
+	}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
-int main() {
-    
-    printf("Size of a char: %lu byte(s)\n", sizeof(char));
-    printf("Size of an int: %lu byte(s)\n", sizeof(int));
-    printf("Size of long int: %lu byte(s)\n", sizeof(long int));
-    printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
-    printf("Size of float: %lu byte(s)\n", sizeof(float));
-    
-    return 0;
-}
+
+int main(void)
+	{
+
+		printf("Size of a char: %lu byte(s)\n", sizeof(char));
+		printf("Size of an int: %lu byte(s)\n", sizeof(int));
+		printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+		printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
+		printf("Size of float: %lu byte(s)\n", sizeof(float));
+
+		return (0);
+
+	}
