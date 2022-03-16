5354238ee72a8b176a164ad4835cd5132aee7bd8
PCRepairDoctor <93010309+devpcrd@users.noreply.github.com>
2022-03-16 06:03:29 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,79 +5,46 @@
  */
 void print_times_table(int n)
 {
-int a = 0, rep, b;
+int x, y, z, k, t;
 
-if (n < 0 || n > 15)
-
-	return;
-
-while (a <= n)
-
-{
-
-	for (b = 0; b <= n; b++)
-
+	for (x = 0; x <= n; x++)
 	{
-
-		rep = a * b;
-
-		if (b == 0)
-
-			_putchar('0' + rep);
-
-		else if (rep < 10)
-
+		for (y = 0; y <= n; y++)
 		{
+			z = x * y;
+			if (z == 0 && y == 0)
+				_putchar(z + '0');
+			else if (z > 99)
+			{
+				k = z % 10;
+				t = (z / 10) % 10;
+				z /= 100;
+				_putchar(',');
+				_putchar(' ');
+				_putchar(z + '0');
+				_putchar(t + '0');
+				_putchar(k + '0');
+			}
+			else if (z > 9)
+			{
+				k = z % 10;
+				z /= 10;
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+				_putchar(k + '0');
 
-			_putchar(' ');
-
-			_putchar(' ');
-
-			_putchar('0' + rep);
-
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
 		}
-
-		else if (rep < 100)
-
-		{
-
-			_putchar(' ');
-
-			_putchar('0' + rep / 10);
-
-			_putchar('0' + rep % 10);
-
-		}
-
-		else
-
-		{
-
-			_putchar('0' + rep / 100);
-
-			_putchar('0' + (rep - 100) / 10);
-
-			_putchar('0' + rep % 10);
-
-		}
-
-		if (b < n)
-
-		{
-
-			_putchar(',');
-
-			_putchar(' ');
-
-		}
-
+		_putchar('\n');
 	}
-
-	_putchar('\n');
-
-	a++;
-
 }
-	
-}
-
