c6c188a5c1d532e4ce2f5734ab8b6ce61ecd2a68
root <PCRepairDoctor>
2022-03-17 11:45:15 -0700

    gddgu

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,7 +15,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 	{
-		if (month >= 2 && day >= 60)
+		if (month > 2 && day >= 60)
 		day++;
 
 	    printf("Day of the year: %d\n", day);
@@ -25,9 +25,12 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Today is %dth February %d\n", day - 31, year);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else
+		{
 			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
