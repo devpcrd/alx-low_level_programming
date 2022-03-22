af35e26b4f7da3e2d4a862221b13aaf58376b64f
PCRepairDoctor <93010309+devpcrd@users.noreply.github.com>
2022-03-22 11:33:37 +0100

    Task 4. Prints a string in reverse order

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_rev - print a string in reverse followed by a new line
+ * print_rev - print a string in reverse order followed by a new line
  * @s: string to be reversed
  */
 
