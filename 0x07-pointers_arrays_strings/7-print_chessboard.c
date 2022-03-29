6222910bfa36c6314966833fed76212763b0e16d
PCRepairDoctor <93010309+devpcrd@users.noreply.github.com>
2022-03-29 17:19:53 +0100

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,8 +11,8 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-			putchar(a[i][j]);
+			_putchar(a[i][j]);
 
-		putchar('\n');
+		_putchar('\n');
 	}
 }
