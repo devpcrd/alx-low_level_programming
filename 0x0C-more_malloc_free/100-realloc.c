5e8a6530d4b3a97e96e46a9e4f3d4dad7cf70941
PCRepairDoctor <93010309+devpcrd@users.noreply.github.com>
2022-04-08 21:40:35 +0100

    fix betty

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,38 +1,33 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * _realloc - reallocates a memory block using malloc and free (realloc)
+ * _realloc -  reallocates a memory block using malloc and free
  * @ptr: pointer
- * @old_size: size of old pointer
- * @new_size: size of new pointer
- * Return: new void pointer or NULL if it fails
+ * @old_size: old size
+ * @new_size: new size
+ * Return: pointer
  */
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *clone, *relloc;
 	unsigned int i;
 
 	if (ptr != NULL)
-		clone = ptr;
+	clone = ptr;
 	else
-		return (malloc(new_size)); 
+	{ return (malloc(new_size)); }
 	if (new_size == old_size)
-		return (ptr);
+	return (ptr);
 	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (0);
-	}
+	{ free(ptr);
+	return (0); }
 	relloc = malloc(new_size);
 	if (relloc == NULL)
-		return (0);
+	return (0);
 	for (i = 0; i < (old_size || i < new_size); i++)
 	{
 		*(relloc + i) = clone[i];
 	}
 	free(ptr);
-	return (relloc);
+return (relloc);
 }
