2a2a9ebd3122ac1a3f87ade5410b2234c1e5f2f7
root <PCRepairDoctor>
2022-04-21 16:41:34 -0700

    add every file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list of type list_t
+ * @h: pointer to first elememt of a list of type list_t
+ *
+ * Return: no of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
