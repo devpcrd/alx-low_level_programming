39e1d2a3df0ac42023c5b5e5df515ce4e23bbd83
PCRepairDoctor <93010309+devpcrd@users.noreply.github.com>
2022-04-22 01:15:54 +0100

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,14 @@
-#include "lists.h"
 #include <stdio.h>
-/**
- *  liebre_tortuga -function that executes before main
- **/
-void liebre_tortuga(void)
-{
-	char *string1, *string2;
 
-	string1 = "You're beat! and yet, you must allow,\n";
-	string2 = "I bore my house upon my back!\n";
-	printf("%s%s", string1, string2);
+void __attribute__((constructor)) print_first(void);
+
+/**
+ * print_first - Prints a string before the
+ *        main function is executed.
+ */
+
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
