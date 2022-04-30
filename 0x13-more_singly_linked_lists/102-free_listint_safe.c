d39f0c42a6d7582e0bbc7b1abb096800de93a4df
PCRepairDoctor <93010309+devpcrd@users.noreply.github.com>
2022-04-30 10:44:07 +0100

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -7,22 +7,22 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t counter = 0;
-	listint_t **safe_node = **h;
-	listint_t **tmp_node;
+        size_t counter = 0;
+        listint_t *safe_node = *h;
+        listint_t *tmp_node;
 
-	if (!h)
-		return (0);
-	while (safe_node != NULL)
-	{
-		counter++;
-		tmp_node = safe_node;
-		safe_node = safe_node->next;
-		free(tmp_node);
+        if (!h)
+                return (0);
+        while (safe_node != NULL)
+        {
+                counter++;
+                tmp_node = safe_node;
+                safe_node = safe_node->next;
 
-		if (tmp_node < safe_node)
-			break;
-	}
-	**h = NULL;
-	return (counter);
+                if (tmp_node < safe_node)
+                        break;
+                free(tmp_node);
+        }
+        *h = NULL;
+        return (counter);
 }
