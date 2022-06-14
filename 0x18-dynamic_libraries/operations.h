f238ae6ec4a35a5c74fedd5a57624802da7c851c
devpcrd <infinitepeco@gmail.com>
2022-06-14 09:03:37 +0530

    c dynamic lib callable from python

---
+++ 0x18-dynamic_libraries/operations.h
@@ -0,0 +1,9 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+int add(int a, int b);
+int sub(int a, int b);                                        int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif
